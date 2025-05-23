5fa8d54dd52dbb2c6025e0e0c32202010985af50
EvgenShmyrin <shmirin@gmail.com>
2025-05-23 18:15:47 +0300

    test1-1 part2

--- test1/HTML_application.js
+++ test1/HTML_application.js
@@ -1,17 +1,72 @@
 let input = document.forms['myForm'].input
 let inputButton = document.getElementById('inputButton')
 let monitor = document.getElementById('monitor');
+let arrTest = []
+let reverseArrTest = []
+
+
 inputButton.addEventListener("click", function (event) {
     event.preventDefault();
     let text = input.value;
     let match = text.match(/^\s*([a-zA-Z0-9]+)\s*=\s*([a-zA-Z0-9]+)\s*$/);
-    // console.log(match);
-
-    // console.log(match[0]);
-    if (match === 0) {
-        monitor.append(`Помилка`);
-
-    }else {
-        monitor.append(match[0]);
+    let p = document.createElement('p');
+    let oneArr = {
+        name: match[1],
+        value: match[2],
+    };
+    reverseArrTest.push(oneArr);
+    arrTest = reverseArrTest.reverse();
+    // console.log(oneArr)
+    console.log(arrTest)
+    monitor.innerText = '';
+    for (let el of arrTest) {
+        console.log(el)
+        console.log(`${el.name}=${el.value}`)
+        let p = document.createElement('p');
+        p.textContent = `${el.name}=${el.value}`;
+        monitor.appendChild(p);
     }
+})
+let button1 = document.getElementById('button1');
+button1.addEventListener("click", function (event) {
+    event.preventDefault();
+    let button1p = document.createElement('p');
+    button1p.id = 'button1p';
+    let sort = arrTest.sort(function (a, b) {
+        return a.name - b.name;
+    })
+    monitor.innerText = '';
+    monitor.append(button1p);
+    for (let el of sort) {
+        let button1p = document.createElement('p');
+        button1p.textContent = `${el.name}=${el.value}`;
+        monitor.appendChild(button1p);
+    }
+    console.log(sort)
+})
+let button2 = document.getElementById('button2');
+button2.addEventListener("click", function (event) {
+    event.preventDefault();
+    let button2p = document.createElement('p');
+    let sort = arrTest.sort(function (a, b) {
+        // console.log(a.name, b.name);
+        return a.value - b.value;
+    })
+    monitor.innerText = '';
+    monitor.append(button2p);
+    for (let el of sort) {
+
+        button2p.innerText = `${el.name}=${el.value}`;
+        monitor.append(button2p);
+    }
+    console.log(sort)
+})
+let button3 = document.getElementById('button3');
+button3.addEventListener("click", function (event) {
+    // event.preventDefault();
+    let button3p = document.createElement('p');
+    button3p.id = 'button3p';
+    monitor.innerText = '';
+    monitor.append(button3p);
+
 })
