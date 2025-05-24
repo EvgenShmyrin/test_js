fe1f9fb3621cdb7ee54a5f63de29e9c9ddc6f5cc
EvgenShmyrin <shmirin@gmail.com>
2025-05-24 15:47:13 +0300

    test1-1 part3

--- test1/HTML_application.js
+++ test1/HTML_application.js
@@ -3,21 +3,15 @@ let inputButton = document.getElementById('inputButton')
 let monitor = document.getElementById('monitor');
 let arrTest = []
 let reverseArrTest = []
-
-
 inputButton.addEventListener("click", function (event) {
     event.preventDefault();
     let text = input.value;
     let match = text.match(/^\s*([a-zA-Z0-9]+)\s*=\s*([a-zA-Z0-9]+)\s*$/);
-    let p = document.createElement('p');
     let oneArr = {
         name: match[1],
         value: match[2],
     };
-    reverseArrTest.push(oneArr);
-    arrTest = reverseArrTest.reverse();
-    // console.log(oneArr)
-    console.log(arrTest)
+    arrTest.unshift(oneArr);
     monitor.innerText = '';
     for (let el of arrTest) {
         console.log(el)
@@ -26,6 +20,7 @@ inputButton.addEventListener("click", function (event) {
         p.textContent = `${el.name}=${el.value}`;
         monitor.appendChild(p);
     }
+    input.value = '';
 })
 let button1 = document.getElementById('button1');
 button1.addEventListener("click", function (event) {
@@ -33,7 +28,7 @@ button1.addEventListener("click", function (event) {
     let button1p = document.createElement('p');
     button1p.id = 'button1p';
     let sort = arrTest.sort(function (a, b) {
-        return a.name - b.name;
+        return a.name.localeCompare(b.name);
     })
     monitor.innerText = '';
     monitor.append(button1p);
@@ -48,14 +43,14 @@ let button2 = document.getElementById('button2');
 button2.addEventListener("click", function (event) {
     event.preventDefault();
     let button2p = document.createElement('p');
+    button2p.id = 'button2p';
     let sort = arrTest.sort(function (a, b) {
-        // console.log(a.name, b.name);
-        return a.value - b.value;
+        return a.value.localeCompare(b.value);
     })
     monitor.innerText = '';
     monitor.append(button2p);
     for (let el of sort) {
-
+        let button2p = document.createElement('p');
         button2p.innerText = `${el.name}=${el.value}`;
         monitor.append(button2p);
     }
@@ -63,7 +58,6 @@ button2.addEventListener("click", function (event) {
 })
 let button3 = document.getElementById('button3');
 button3.addEventListener("click", function (event) {
-    // event.preventDefault();
     let button3p = document.createElement('p');
     button3p.id = 'button3p';
     monitor.innerText = '';
